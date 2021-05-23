e4076313b8ce012f88954ff64e4ea1a7b5447619
Thomas Creedon <tom@tomsWeb.consulting>
2021-05-22 18:59:26 -0700

    twcsl.js initial version

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
 # Tom's Web Consulting Squarespace Library
+
+This is a [monorepo](https://en.wikipedia.org/wiki/Monorepo) of code for adding
+various effects to Squarespace v7.0 and v7.1 sites.
+
+## General Layout Overview
+
+* v7.0 and v7.1 effects
+
+* v7.0 only effects
+
+  * effects for all v7.0 templates
+  
+  * template specific effects
+  
+* v7.1 only effects
+
+* twcsl
+
+To find an effect browse the repository. Each effect has it's own version
+number, code, read me, and etc.
+
+**The rest of this document covers twcsl**.
+
+# twcsl
+
+Is a Javascript library of common properties and methods useful for building
+higher level effects in Squarespace sites.
+
+### Version 0.1d0
+
+## Quick Install
+
+Add the following to Settings > Advanced > Code Injection > HEADER.
+
+```html
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://d1j8mu9lowy9zf.cloudfront.net/twcsl/0.1d0/twcsl.js"></script>
+```
+
+## Example
+
+You want to build an effect that does something when you are on a store category
+product grid page. You can use twcsl to test if you are on such a page with the
+following.
+
+```javascript
+if ( twcsl.storePage.hasCategory ) console.log ( 'has category' );
+```
+
+## Changes
+
+<!-- * **2021-05-19**
+<br><br>
+  * added a choice of paragraph styles
+  * user can set store url slug
+  * bumped version to 0.2d0
+  <br><br -->
+* **2021-05-22**
+<br><br>
+  * initial version
---
+++ twcsl.js
@@ -0,0 +1,216 @@
+const twcsl = ( ( ) => {
+
+  /*
+  
+  Tom's Web Consulting Squarespace Library
+  
+  SS Versions   : 7.0, 7.1
+  
+  Version       : 0.1d0
+  
+  Dependancies  : jQuery
+  
+  By            : Thomas Creedon < http://www.tomsWeb.consulting/ >
+  
+  */
+  
+  'use strict';
+  
+  // begin private properties
+  
+    const _ssVersion = Static.SQUARESPACE_CONTEXT.templateVersion;
+    
+    const _is70 = _ssVersion == '7.0';
+    
+    const _is71 = _ssVersion == '7.1';
+    
+    var _storePage = {
+    
+      category : '',
+      
+      hasCategory : false,
+      
+      hasTag : false,
+      
+      isDetail : false,
+      
+      isStorePage : $( 'body' ).hasClass ( 'collection-type-products' ),
+      
+      tag : '',
+      
+      urlSlug : '',
+      
+      };
+      
+    // end private properties
+    
+  // begin private methods
+  
+    ( ( o ) => { // initialize storePage
+    
+      o.isStorePage = $( 'body' ).hasClass ( 'collection-type-products' );
+      
+      if ( ! o.isStorePage ) return;
+      
+      o.isDetail = $( '[id^="item-"]' ).length != 0;
+      
+      o.urlSlug = Static.SQUARESPACE_CONTEXT.collection.fullUrl;
+        
+      // begin tag
+      
+        o.tag = new URLSearchParams ( location.search )
+        
+          .get ( 'tag' );
+          
+        if ( o.tag === null ) o.tag = '';
+        
+        // end tag
+        
+      o.hasTag = o.tag != '';
+      
+      o.category = ( ( ) => {
+      
+        if ( o.isDetail ) return ''; // bail if product detail page
+        
+        if ( o.hasTag ) return ''; // bail if tag page
+        
+        let category;
+        
+        switch ( true ) {
+        
+          case _is70 :
+          
+            category = new URLSearchParams ( location.search )
+            
+              .get ( 'category' );
+              
+            if ( category === null ) category = undefined;
+            
+            break;
+            
+          case _is71 :
+          
+            category = location
+            
+              .pathname
+              
+              .replace ( o.urlSlug + '/', '' );
+              
+            break;
+            
+          }
+          
+        return category;
+        
+        } ) ( );
+        
+      o.hasCategory = o.category != '';
+      
+      } ) ( _storePage );
+    
+    // end private methods
+    
+  // begin public properties
+  
+    // end public properties
+    
+  // begin public methods
+  
+    // begin scroll effect
+    
+      const scrollEffect = ( easingFunction = 'linear', invertScale = false,
+      
+        callback ) => {
+        
+        if ( typeof callback != 'function' ) return; // bail if no callback
+        
+        const calculateRatio = ( n, whole, easingFunction, invertScale) => {
+        
+          let ratio = n / whole;  // 0 to 1
+          
+          switch ( easingFunction ) {
+          
+            case 'inCircle' :
+            
+              // < https://easings.net/#easeInCirc >
+              
+              ratio = 1 - Math.sqrt ( 1 - Math.pow ( ratio, 2 ) );
+              
+              break;
+              
+            case 'inOutQuadratic' :
+            
+              // < https://easings.net/#easeInOutQuad >
+              
+              ratio = ratio < 0.5 ? 2 * ratio * ratio :
+              
+                1 - Math.pow ( -2 * ratio + 2, 2 ) / 2;
+                
+              break;
+              
+            case 'outCircle' :
+            
+              // < https://easings.net/#easeOutCirc >
+              
+              ratio = Math.sqrt ( 1 - Math.pow ( ratio - 1, 2 ) );
+              
+              break;
+              
+            }
+            
+          if ( invertScale ) ratio = 1 - ratio; // 1 to 0
+          
+          return ratio;
+          
+          } // end calculateRatio
+          
+        let maxScrollTop;
+        
+        $( window )
+        
+          .resize ( function ( ) {
+          
+            maxScrollTop = $( document ).height ( ) - $( window ).height ( );
+            
+            } )
+            
+          .resize ( )
+          
+          .scroll ( function ( ) {
+          
+            const ratio = calculateRatio ( $( window ).scrollTop ( ),
+            
+              maxScrollTop, easingFunction, invertScale );
+              
+            callback ( ratio );
+            
+            } );
+        };
+        
+        // end scroll effect
+
+    // end public methods
+    
+  return {
+  
+    // begin properties
+    
+      is70      : _is70,
+      
+      is71      : _is71,
+      
+      ssVersion : _ssVersion,
+      
+      storePage : _storePage,
+      
+      // end properties
+      
+    // begin methods
+    
+      scrollEffect : scrollEffect,
+      
+      // end methods
+      
+    };
+    
+  } ) ( );
