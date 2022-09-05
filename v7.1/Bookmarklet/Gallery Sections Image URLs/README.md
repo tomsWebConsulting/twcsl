994f559405a603b4d62709a9c7914c4cdcff000d
Thomas Creedon <tom@tomsWeb.consulting>
2022-09-05 14:31:36 -0700

    initial version

---
+++ v7.1/Bookmarklet/Gallery Sections Image URLs/README.md
@@ -0,0 +1,40 @@
+# Gallery Sections Image URLs
+
+### Version 0.1.0
+
+#### SS Version 7.1
+
+#### Fluid Engine Compatible : Unknown
+
+---
+
+## Quick Install
+
+* Add code from file **[gallery sections image urls.bookmarklet][1]** as the URL
+  for a bookmark in your browser named **Gallery Sections Image URLs**.
+  
+* Go to a page in a site with gallery sections and use the bookmarklet to get
+  the URLs of the images.
+
+## What is a bookmarket?
+
+Wikipedia has a nice entry on [bookmarklet][2].
+
+## Make a Donation
+
+Please consider [making a donation][3].
+
+## Changes
+
+<!-- * **2022-05-15**
+
+  * fix for when site is not in config
+  * bumped version to v0.1.1
+  -->
+* **2022-09-04**
+
+  * initial version
+
+[1]: gallery%20sections%20image%20urls.bookmarklet#L1
+[2]: https://en.wikipedia.org/wiki/Bookmarklet
+[3]: https://github.com/tomsWebConsulting/twcsl#make-a-donation
---
+++ v7.1/Bookmarklet/Gallery Sections Image URLs/gallery sections image urls.js
@@ -0,0 +1,91 @@
+( function ( ) {
+
+  /*
+  
+    gallery sections image urls
+    
+    Version       : 0.1.0
+    
+    SS Version    : 7.1
+    
+    Fluid
+    Engine
+    Compatible    : Unknown
+    
+    By            : Thomas Creedon < http://www.tomsWeb.consulting/ >
+    
+    no user serviceable parts below
+    
+    */
+    
+  const l = document
+  
+    .querySelectorAll ( '.squarespace-config' )
+    
+    .length;
+    
+  const className = 'gallery-section';
+  
+  let siteDocument;
+  
+  let urls = '';
+  
+  // set site document
+  
+  if ( l ) {
+  
+    siteDocument = document
+    
+    .querySelector ( 'iframe#sqs-site-frame' )
+    
+    .contentDocument;
+    
+    } else {
+    
+      siteDocument = document;
+      
+      }
+      
+  siteDocument
+  
+    .getElementsByClassName ( className )
+    
+    .forEach ( gallerySection => {
+    
+      const dataSectionId = gallerySection.getAttribute ( 'data-section-id' );
+      
+      const selector = '.gallery-grid .gallery-grid-item-wrapper img';
+      
+      urls += `data section id : ${ dataSectionId }\n\n`;
+      
+      gallerySection
+      
+        .querySelectorAll ( selector )
+        
+        .forEach ( image => {
+        
+          const url = image.getAttribute ( 'data-src' );
+          
+          urls += url + '\n';
+          
+          } );
+          
+      urls += '\n\n';
+      
+      } );
+  
+  if ( ! urls ) return; // bails if no urls
+  
+  const html = `<pre><code>${ urls }</code></pre>`;
+  
+  const w = window.open ( '' );
+  
+  w.document.write ( html );
+  
+  const title = document.createElement ( 'title' );
+  
+  title.append ( 'Gallery Sections Image URLs' );
+  
+  w.document.head.append ( title );
+  
+  } ( ) );
