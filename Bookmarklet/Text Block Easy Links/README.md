af4141fa0fcd28ee72b260fc9d3c379dff4277f9
Thomas Creedon <tom@tomsWeb.consulting>
2023-01-16 13:30:01 -0800

    initial version

---
+++ Bookmarklet/Text Block Easy Links/README.md
@@ -0,0 +1,51 @@
+# Text Block Easy Links
+
+### [License][1]
+
+### Version 0.1.0
+
+---
+
+## Quick Install
+
+* Add code from file **[text block easy links.bookmarklet][2]** as the URL for a
+  bookmark in your browser named **Text Block Easy Links**.
+  
+* Paste text into a text block in the following format.
+
+  ```text
+  My first line of text;http://www.tomsWeb.consulting/
+  My second line of text;http://www.squarespace.com/
+  ```
+  
+  Notice that each line has your link text semicolon and the URL for the link.
+  Lines without a semicolons will be bypassed for being made links.
+  
+## Note
+
+This bookmarklet is useful for very specific situations, a text block where you
+want to enter a lot of lines of text that you want to be links.
+
+## What is a bookmarket?
+
+Wikipedia has a nice entry on [bookmarklet][3].
+
+## Make a Donation
+
+Please consider [making a donation][4].
+
+## Changes
+
+<!-- * **2022-05-15**
+
+  * fix for when site is not in config
+  * bumped version to v0.1.1
+  -->
+* **2023-01-16**
+
+  * initial version
+
+[1]: text%20block%20easy%20links.bookmarklet#L1
+[2]: https://en.wikipedia.org/wiki/Bookmarklet
+[3]: https://github.com/tomsWebConsulting/twcsl#make-a-donation
+[4]: https://github.com/tomsWebConsulting/twcsl/blob/main/LICENSE.txt#L1
---
+++ Bookmarklet/Text Block Easy Links/text block easy links.js
@@ -0,0 +1,77 @@
+( function ( ) {
+
+  /*
+  
+    text block easy links
+    
+    License       : < https://bit.ly/3F8sn8W >
+    
+    Version       : 0.1.0
+    
+    By            : Thomas Creedon < http://www.tomsWeb.consulting/ >
+    
+    no user serviceable parts below
+    
+    */
+    
+  const l = document
+  
+    .querySelectorAll ( '.squarespace-config' )
+    
+    .length;
+    
+  let siteDocument;
+  
+  // set site document
+  
+  if ( ! l ) return; // bail if not in Preview
+  
+    siteDocument = document
+    
+    .querySelector ( 'iframe#sqs-site-frame' )
+    
+    .contentDocument;
+    
+  const selector =
+  
+    '.sqs-block-html .sqs-html-content[contenteditable="true"] p';
+    
+  let nodes = siteDocument
+  
+    .querySelectorAll ( selector );
+    
+  if ( ! nodes.length ) return; // bail if no text block edit
+  
+  [
+  
+    ...nodes
+      
+      ]
+      
+      .filter ( n => ! n.querySelector ( 'a' ) )
+      
+      .forEach ( ( node ) => {
+      
+        // console.log ( node );
+        
+        let html = node.innerHTML
+        
+          .split ( ';' );
+          
+        if ( html.length == 1 ) return; // contine if no url
+        
+        // console.log ( html );
+        
+        const url = html [ 1 ];
+        
+        html = html [ 0 ];
+        
+        node.innerHTML = `<a href="${ url }">
+        
+          ${ html }
+          
+          </a>`;
+          
+        } );
+        
+  } ( ) );
