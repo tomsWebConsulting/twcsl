c86a2a9740d6cb8e8d02a3cac8dec033711b601d
Thomas Creedon <tom@tomsWeb.consulting>
2023-08-10 17:29:55 -0700

    initial version

---
+++ v7.1/Page/Events/Event/Events Page Event Thumbnail Add/README.md
@@ -0,0 +1,103 @@
+# Events Page Event Thumbnail Add
+
+### [License][1]
+    
+### Synopsis
+
+Use settings thumbnail as thumbnail for Event.
+
+### Version
+
+  * 0.1.0
+
+#### SS Version
+
+  * 7.1
+
+#### Dependencies
+
+  * [Squarespace plan][2] that supports [JavaScript][3].
+  
+  * [Tom's Web Consulting Squarespace Library][4]
+
+#### Fluid Engine Compatible
+
+  * Not Applicable
+
+---
+
+## Install
+
+* Install [twcsl][5]. After you install twcsl be sure to continue on with the
+  rest of the steps.
+  
+* Add code from file **[events page event thumbnail add.html][6]** to Events
+  Settings > Advanced > Page Header Code Injection for the events page.
+  Please see [Per-page code injection][7]. Read the code for any
+  instructions within.
+
+## Bookmarklet
+
+By default the code adds the thumbnail to the meta information area of an event.
+If you want the thumbnail to be added at the top of the content of the event
+follow these steps.
+
+* Create an event to use as a template. Add an inline image block at the top of
+  the event.
+  
+* Set the image block link to **#twc-epefihg**.
+
+* Save the event and exit.
+  
+You can use spacers to help control the size and spacing of the image. Keep the
+formatting really simple or things may break. This post is only a template.
+
+* Add code from file **[events page event first image html
+  get.bookmarklet][8]** as the URL for a bookmark in your browser named
+  **Events Page Event First Image Html Get**.
+
+**What is a bookmarket?**
+
+Wikipedia has a nice entry on [bookmarklet][9].
+
+* In the event template you created select the **Events Page Event First Image
+  Html Get** bookmark.
+  
+A window/tab should pop-up with HTML code in it. If the pop-up doesn't appear be
+sure that you don't have pop-up or ad blocker software that is causing an issue.
+
+* Copy all the HTML in the pop-up to your clipboard.
+
+* Edit the **Events Page Event Thumbnail Add** code you installed previously and
+  paste the HTML where indicated in the code.
+
+## Notes
+
+This effect is not active in SS Preview to test it use [private browsing][10].
+
+## Make a Donation
+
+Please consider [making a donation][11].
+
+## Changes
+
+<!-- * **2023-05-30**
+
+  * remove errants break statement, no functional changes
+  * bumped version to 0.1.1
+  -->
+* **2023-08-10**
+
+  * initial version
+
+[1]: https://github.com/tomsWebConsulting/twcsl/blob/main/LICENSE.txt#L1
+[2]: https://www.squarespace.com/pricing
+[3]: https://en.wikipedia.org/wiki/JavaScript
+[5]: https://github.com/tomsWebConsulting/twcsl#install-options
+[4]: https://github.com/tomsWebConsulting/twcsl
+[6]: events%20page%20event%20thumbnail%20add.html#L1
+[7]: https://support.squarespace.com/hc/en-us/articles/205815908-Using-code-injection#toc-per-page-code-injection
+[8]: events%20page%20event%20first%20image%20html%20get.bookmarklet#L1
+[11]: https://github.com/tomsWebConsulting/twcsl#make-a-donation
+[10]: https://support.squarespace.com/hc/en-us/articles/207099587-Using-private-browsing-or-incognito-mode
+[9]: https://en.wikipedia.org/wiki/Bookmarklet
---
+++ v7.1/Page/Events/Event/Events Page Event Thumbnail Add/events page event first image html get.js
@@ -0,0 +1,181 @@
+( function ( ) {
+
+  /*
+  
+    events page event first image html get
+    
+    License       : < https://tinyurl.com/s872fb68 >
+    
+    Version       : 0.1.0
+    
+    SS Version    : 7.1
+    
+    Fluid
+    Engine
+    Compatible    : Not Applicable
+    
+    By            : Thomas Creedon < http://www.tomsWeb.consulting/ >
+    
+    no user serviceable parts below
+    
+    */
+    
+  debugger;
+  
+  const codeKey = 'twc-epefihg';
+  
+  const codeTitle = 'Events Page Event First Image Html Get';
+  
+  const version = '0.1.0';
+  
+  const s = 'Tom\'s Web Consulting < http://www.tomsWeb.consulting >, ' +
+  
+    `${ codeTitle } Bookmarklet v${ version }, ` +
+    
+    'License < https://tinyurl.com/s872fb68 >';
+    
+  console.log ( s );
+  
+  const l = document
+  
+    .querySelectorAll ( '.squarespace-config' )
+    
+    .length;
+    
+  let selector = '.eventitem-column-content .sqs-block-image ' +
+  
+    `.design-layout-inline .sqs-block-image-link[ href="#${ codeKey }" ]`;
+    
+  let siteDocument;
+  
+  // set site document
+  
+  if ( l ) {
+  
+    siteDocument = document
+    
+    .querySelector ( 'iframe#sqs-site-frame' )
+    
+    .contentDocument;
+    
+    } else
+    
+      siteDocument = document;
+      
+  let link = siteDocument
+  
+    .querySelector ( selector );
+    
+  if ( link == null ) return; // bail if no tagged image block
+  
+  const escapeHTML = s => {
+  
+    s = new Option ( s )
+    
+      .innerHTML;
+      
+    return s;
+    
+    };
+    
+  selector = '.image-block-wrapper';
+  
+  let element = link
+  
+    .querySelector ( selector );
+    
+  const figure = element
+    
+    .parentElement
+    
+    .parentElement;
+    
+  figure.setAttribute ( 'style', 'max-width : [ width ]px' );
+    
+  figure.append ( element );
+  
+  link.remove ( );
+  
+  element = element
+  
+    .closest ( '.sqs-block-image' );
+    
+  const img = element
+  
+    .querySelector ( 'img' );
+    
+  img.setAttribute ( 'data-image', '[ url ]' )
+  
+  img.setAttribute ( 'data-image-dimensions', '[ width ]x[ height ]' )
+  
+  img.setAttribute ( 'data-src', '[ url ]' )
+  
+  img.setAttribute ( 'height', '[ height ]' )
+  
+  img.setAttribute ( 'src', '[ url ]' )
+  
+  img.setAttribute ( 'width', '[ width ]' )
+  
+  const b = element
+  
+    .parentElement
+    
+    .classList
+    
+    .contains ( 'col' )
+    
+  if ( b )
+  
+    element = element
+    
+      .closest ( '.sqs-row' );
+      
+  let html = element
+  
+    .outerHTML
+    
+    .replace ( / +/g, ' ' )
+    
+    .replace ( / +\n/g, '\n' )
+    
+    .replace ( /\n +/g, '\n' )
+    
+    .replace ( /\n+/g, '\n' )
+    
+    .replaceAll ( '"\n', '"' )
+    
+    .replaceAll ( '\n"', '"' )
+    
+    .replace ( / id="[^"]+"/g, '' )
+    
+    .replace ( /https:\/\/images.squarespace-cdn.com\/content\/v1\/[^?]+/g, '[ url ]' )
+    
+    .replaceAll ( '\n', '\n    ' );
+    
+  html = escapeHTML ( html );
+  
+  html = `<pre><code>    ${ html }\n    </code></pre>`;
+  
+  const w = window.open ( );
+  
+  const title = document
+  
+    .createElement ( 'title' );
+    
+  title.text = `${ codeTitle } Bookmarklet`;
+  
+  w
+  
+    .document
+    
+    .head
+    
+    .append ( title );
+    
+  w
+  
+    .document
+    
+    .write ( html );
+    
+  } ) ( );
