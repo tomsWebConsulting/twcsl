aee00b1029049cfb8098835e3b8c058ead144b84
Thomas Creedon <tom@tomsWeb.consulting>
2025-03-19 20:58:03 -0700

    initial version

---
+++ v7.1/Bookmarklet/Page/Blog/Post/Blog Page Post Tags Add/README.md
@@ -0,0 +1,67 @@
+# Blog Page Post Tags Add
+
+### [License][1]
+
+### Synopsis
+
+Add a comma separated string as tags to a blog post.
+
+### Version
+
+  * 0.1.0
+
+#### SS Version
+
+  * 7.1
+
+#### v7.1 Fluid Engine Compatible
+
+  * Not Applicable
+
+---
+
+## Quick Install
+
+Add code from file **[blog page post tags add.bookmarklet][2]** as the URL for a
+bookmark in your browser named **TWC Blog Page Post Tags Add**.
+
+## How to Use
+
+* Prepare a comma separated string of tags and get it into your clipboard.
+
+* Edit a blog post settings brining up the **Tags** option.
+
+* Paste the clipboard contents into the **Add tag here** field.
+
+* Use the bookmarklet.
+
+* Wait for the bookmarklet to enter your tags.
+
+## Note
+
+You can include the comma character in your tags by using typing **\,**
+(backslash comma).
+
+## What is a bookmarklet?
+
+Wikipedia has a nice entry on [bookmarklet][3].
+
+## Make a Donation
+
+Please consider [making a donation][4].
+
+## Changes
+
+<!-- * **2021-05-12**
+
+  * add support for guard processor
+  * bumped version to 0.2d0
+  -->
+* **2025-03-19**
+
+  * initial version
+
+[1]: https://github.com/tomsWebConsulting/twcsl/blob/main/LICENSE.txt#L1
+[2]: blog%20page%20post%20tags%20add.bookmarklet#L1
+[3]: https://en.wikipedia.org/wiki/Bookmarklet
+[4]: https://github.com/tomsWebConsulting/twcsl#make-a-donation
---
+++ v7.1/Bookmarklet/Page/Blog/Post/Blog Page Post Tags Add/blog page post tags add.js
@@ -0,0 +1,211 @@
+( ( ) => {
+
+  /*
+  
+    blog page post tags add
+    
+    License     : < https://tinyurl.com/s872fb68 >
+    
+    Version     : 0.1.0
+    
+    Copyright   : 2025 Thomas Creedon
+                  
+                  Tom's Web Consulting < http://www.tomsWeb.consulting/ >
+    
+    no user serviceable parts below
+    
+    */
+    
+  const
+  
+    title = 'Blog Page Post Tags Add',
+    
+    version = '0.1.0',
+  
+    s = `${ title } v${ version }
+    
+      License < https://tinyurl.com/s872fb68 >
+      
+      © 2025 Thomas Creedon
+      
+      Tom's Web Consulting < http://www.tomsWeb.consulting >`
+      
+      .replace ( /^\s+/gm, '' );
+      
+  console.log ( s );
+  
+  const
+  
+    codeKey = 'twc-bppta',
+    
+    returnEvent = new KeyboardEvent (
+    
+      'keydown',
+      
+        {
+        
+          bubbles : true,
+          
+          key : 'Enter',
+          
+          keyCode : 13,
+          
+          which : 13
+          
+          }
+          
+      ),
+      
+    xPathEvaluate = ( xPathExpression, contextNode ) => {
+    
+      const xPathResults = document
+      
+        .evaluate (
+        
+          xPathExpression,
+          
+          contextNode,
+          
+          null,
+          
+          XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+          
+          null
+          
+          );
+          
+      return xPathResults;
+      
+      },
+      
+    xPathExpression = `
+    
+      .//div [
+      
+        starts-with (
+        
+          @class,
+          
+          'Transition-panel-'
+          
+          )
+          
+        ]//span [
+        
+          contains (
+          
+            .,
+            
+            'Tags'
+            
+            )
+            
+          ]
+          
+      `,
+      
+    xPathResults =
+    
+      xPathEvaluate ( xPathExpression, document.body );
+      
+  // bail if no element
+  
+  if ( xPathResults.snapshotLength === 0 ) {
+  
+    alert ( `${ codeKey } no Tags panel.` );
+    
+    return;
+    
+    }
+    
+  const
+  
+    callback = ( tag ) => {
+    
+      inputChangeTrigger ( inputElement, tag );
+      
+      inputElement
+      
+        .dispatchEvent ( returnEvent );
+        
+      },
+      
+    inputChangeTrigger = ( node, value = '' ) => {
+    
+      /*
+      
+        based on code by Dan G < https://tinyurl.com/4hes79u8 > and
+        others
+        
+        */
+        
+      const inputTypes = [
+      
+        window.HTMLInputElement,
+        
+        window.HTMLSelectElement,
+        
+        window.HTMLTextAreaElement,
+        
+        ];
+        
+      /*
+      
+        only process the change on elements we know have a value setter in their
+        constructor
+        
+        */
+        
+      const isInputType = inputTypes
+      
+        .indexOf (
+        
+          node.__proto__.constructor
+          
+          )
+          
+        >
+        
+        -1;
+        
+      if ( ! isInputType ) return; // bail if not input type
+      
+      const
+      
+        event = new Event ( 'input', { bubbles : true } ),
+        
+        setValue = Object
+        
+          .getOwnPropertyDescriptor ( node.__proto__, 'value' )
+          
+          .set;
+          
+      setValue.call ( node, value );
+      
+      node.dispatchEvent ( event );
+      
+      },
+      
+    panelElement = xPathResults
+    
+      .snapshotItem ( 0 )
+      
+      .closest ( 'div[ class^="Transition-panel-" ]' ),
+      
+    inputElement = panelElement
+    
+      .querySelector ( 'input' );
+      
+  inputElement
+  
+    .value
+    
+    .replaceAll ( '\\,', '[ comma ]' )
+    
+    .split ( /,\s*/ )
+    
+    .map ( s => s.replaceAll ( '[ comma ]', ',' ) )
+    
+    .forEach ( callback );
+    
+  } ) ( );
