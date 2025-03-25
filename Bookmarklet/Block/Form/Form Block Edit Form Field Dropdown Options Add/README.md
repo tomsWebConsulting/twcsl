771f87507e0cbcdf84ac6bfb4daf4d010f29ef06
Thomas Creedon <tom@tomsWeb.consulting>
2025-03-25 15:13:48 -0700

    fix for apparent changes that Squarespace made that broke the code
    bumped version to 0.1.1

---
+++ Bookmarklet/Block/Form/Form Block Edit Form Field Dropdown Options Add/README.md
@@ -0,0 +1,74 @@
+# Form Block Edit Form Field Dropdown Options Add
+
+### [License][1]
+
+### Synopsis
+
+More easily add options to a Form block Dropdown field.
+
+### Version
+
+  * 0.1.1
+
+#### SS Versions
+
+  * 7.1
+  
+  * 7.0
+
+#### v7.1 Fluid Engine Compatible
+
+  * Not Applicable
+
+---
+
+## Quick Install
+
+Add code from file **[form block edit form field dropdown options
+add.bookmarklet][2]** as the URL for a bookmark in your browser named **TWC Form
+Block Edit Form Field Dropdown Options Add**.
+
+## How to Use
+
+* Add a Dropdown field to a Form block.
+
+* Click on it.
+
+* Click on Options.
+
+* Replace the Option 1 text with a comma separated list of your own options.
+  Type tab to save the option field.
+  
+* Use the bookmarklet.
+
+* Wait for your options to fill in and the bookmarklet to let you know that it
+is done.
+
+## Note
+
+You can include the comma character in your options by using typing **\,**
+(backslash comma).
+
+## What is a bookmarklet?
+
+Wikipedia has a nice entry on [bookmarklet][3].
+
+## Make a Donation
+
+Please consider [making a donation][4].
+
+## Changes
+
+* **2025-03-25**
+
+  * fix for apparent changes that Squarespace made that broke the code
+  * bumped version to 0.1.1
+  
+* **2025-01-17**
+
+  * initial version
+
+[1]: https://github.com/tomsWebConsulting/twcsl/blob/main/LICENSE.txt#L1
+[2]: form%20block%20edit%20form%20field%20dropdown%20options%20add.bookmarklet#L1
+[3]: https://en.wikipedia.org/wiki/Bookmarklet
+[4]: https://github.com/tomsWebConsulting/twcsl#make-a-donation
---
+++ Bookmarklet/Block/Form/Form Block Edit Form Field Dropdown Options Add/form block edit form field dropdown options add.js
@@ -0,0 +1,307 @@
+( async ( ) => {
+
+  /*
+  
+    form block edit form field dropdown options add
+    
+    License     : < https://tinyurl.com/s872fb68 >
+    
+    Version     : 0.1.1
+    
+    Copyright   : 2025 Thomas Creedon
+                  
+                  Tom's Web Consulting < http://www.tomsWeb.consulting/ >
+    
+    no user serviceable parts below
+    
+    */
+    
+  const
+  
+    title = 'Form Block Edit Form Field Dropdown Options Add',
+    
+    version = '0.1.1',
+  
+    s = `${ title } v${ version }
+    
+      License < https://tinyurl.com/s872fb68 >
+      
+      © 2025 Thomas Creedon
+      
+      Tom's Web Consulting < http://www.tomsWeb.consulting >`
+      
+      .replace ( /^\s+/gm, '' );
+      
+  console.log ( s );
+  
+  const
+  
+    delay = ( duration ) => {
+    
+      const
+      
+        callback = ( resolve ) => {
+        
+          setTimeout ( ( ) => resolve ( ), duration );
+          
+          },
+          
+        p = new Promise ( callback );
+        
+      return p;
+      
+      },
+      
+    inputChangeTrigger = ( node, value = '' ) => {
+    
+      /*
+      
+        based on code by Dan G < https://tinyurl.com/4hes79u8 > and
+        others
+        
+        */
+        
+      const inputTypes = [
+      
+        window.HTMLInputElement,
+        
+        window.HTMLSelectElement,
+        
+        window.HTMLTextAreaElement,
+        
+        ];
+        
+      /*
+      
+        only process the change on elements we know have a value setter in their
+        constructor
+        
+        */
+        
+      const isInputType = inputTypes
+      
+        .indexOf (
+        
+          node.__proto__.constructor
+          
+          )
+          
+        >
+        
+        -1;
+        
+      if ( ! isInputType ) return; // bail if not input type
+      
+      const
+      
+        event = new Event ( 'input', { bubbles : true } ),
+        
+        setValue = Object
+        
+          .getOwnPropertyDescriptor ( node.__proto__, 'value' )
+          
+          .set;
+          
+      setValue.call ( node, value );
+      
+      node.dispatchEvent ( event );
+      
+      },
+      
+    processOptions = async ( options, optionsElement, addElement ) => {
+    
+      for ( let i = 0; i < options.length; i++ ) {
+      
+        let element = optionsElement
+        
+          .querySelectorAll ( 'input' )
+          
+          [ i ];
+          
+        if ( element === undefined ) {
+        
+          addElement.click ( );
+          
+          await delay ( 125 );
+          
+          element = [
+          
+            ...
+            
+            optionsElement
+            
+              .querySelectorAll ( 'input' )
+              
+            ]
+            
+            .slice ( -1 )
+            
+            [ 0 ];
+            
+          }
+          
+        inputChangeTrigger ( element, options [ i ] );
+        
+        }
+        
+      };
+      
+    xPathEvaluate = ( xPathExpression, contextNode ) => {
+    
+      const xPathResults = document
+      
+        .evaluate (
+        
+          xPathExpression,
+          
+          contextNode,
+          
+          null,
+          
+          XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+          
+          null
+          
+          );
+          
+      return xPathResults;
+      
+      };
+      
+  try {
+  
+    const
+    
+      transitionContainerSelector =
+      
+        'div[ data-test="block-editor-container" ] ' +
+        
+          'div[ class^="Transition-container-" ]',
+          
+      transitionContainerElement = document
+      
+        .querySelector ( transitionContainerSelector );
+        
+    // bail if no block editor element
+    
+    if ( transitionContainerElement === null )
+    
+      throw new Error ( `there appears to be no form editor dialog.` );
+      
+    let
+    
+      xPathExpression = `
+      
+        .//div [
+        
+          @data-test-id='header-title'
+          
+          ]
+          
+          [
+          
+            contains (
+            
+              .,
+              
+              'Dropdown'
+              
+              )
+              
+            ]
+            
+        `,
+        
+      xPathResults = xPathEvaluate (
+      
+        xPathExpression,
+        
+        transitionContainerElement
+        
+        );
+        
+    // bail if no dropdown element
+    
+    if ( xPathResults.snapshotLength === 0 )
+    
+      throw new Error ( `there appears to be no Dropdown dialog.` );
+      
+    xPathExpression = './/label [ contains ( ., "Options" ) ]';
+    
+    xPathResults = xPathEvaluate (
+    
+      xPathExpression,
+      
+      transitionContainerElement
+      
+      );
+      
+    // bail if no options element
+    
+    if ( xPathResults.snapshotLength === 0 )
+    
+      throw new Error ( `there appears to be no Options.` );
+      
+    const optionsElement = xPathResults
+    
+      .snapshotItem ( 0 )
+      
+      .parentElement;
+      
+    xPathExpression = './/span [ contains ( text ( ), "Add option" ) ]',
+    
+    xPathResults = xPathEvaluate (
+    
+      xPathExpression,
+      
+      optionsElement
+      
+      );
+      
+    // bail if no add element
+    
+    if ( xPathResults.snapshotLength === 0 )
+    
+      throw new Error ( `there appears to be no ADD OPTION button.` );
+      
+    const
+    
+      addElement = xPathResults
+      
+        .snapshotItem ( 0 )
+        
+        .closest ( 'button' ),
+        
+      options = optionsElement
+      
+        .querySelector ( 'input' )
+        
+        .value
+        
+        .replaceAll ( '\\,', '[ comma ]' )
+        
+        .split ( /,\s*/ )
+        
+        .map ( s => s.replaceAll ( '[ comma ]', ',' ) );
+        
+      await processOptions (
+      
+        options,
+        
+        optionsElement,
+        
+        addElement
+        
+        );
+        
+    alert ( `TWC ${ title } is done.` );
+    
+    } catch ( error ) {
+    
+      const s = `TWC ${ title }, ${ error }`;
+      
+      alert ( s );
+      
+    }
+    
+  } ) ( );
