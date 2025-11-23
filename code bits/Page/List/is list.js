96238225f4c66b43ecb34160a9ac7b8431c26643
Thomas Creedon <tom@tomsWeb.consulting>
2025-11-22 23:55:53 -0800

    initial version

---
+++ code bits/Page/List/is list.js
@@ -0,0 +1,38 @@
+// License < https://tinyurl.com/s872fb68 > | © 2025 Thomas Creedon | Tom's Web Consulting < http://www.tomsWeb.consulting/ >
+
+const isList
+
+  =
+  
+  !
+  
+  Static
+  
+    .SQUARESPACE_CONTEXT
+    
+    .item
+    
+    ?.id;
+    
+/*
+
+  Example Usage :
+  
+    if ( ! isList ) return; // bail if not list page
+    
+  
+  License       : < https://tinyurl.com/s872fb68 >
+  
+  Version       : 1.0.0
+  
+  SS Versions   : 7.1, 7.0
+  
+  Copyright     : 2025 Thomas Creedon
+                  
+                  Tom's Web Consulting < http://www.tomsWeb.consulting/ >
+  
+  Changes
+  
+    * initial version
+    
+  */
