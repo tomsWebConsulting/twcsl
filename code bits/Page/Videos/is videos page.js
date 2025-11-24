5b75ffa4a3a955549491510a07dbf3f502d2b5ae
Thomas Creedon <tom@tomsWeb.consulting>
2025-11-23 16:56:00 -0800

    initial version

---
+++ code bits/Page/Videos/is videos page.js
@@ -0,0 +1,36 @@
+// License < https://tinyurl.com/s872fb68 > | © 2025 Thomas Creedon | Tom's Web Consulting < http://www.tomsWeb.consulting/ >
+
+const isVideosPage = Static
+
+  .SQUARESPACE_CONTEXT
+  
+  .collection
+  
+  ?.type
+  
+  ===
+  
+  24;
+  
+/*
+
+  Example Usage :
+  
+    if ( ! isVideosPage ) return; // bail if not videos page
+    
+  
+  License       : < https://tinyurl.com/s872fb68 >
+  
+  Version       : 1.0.0
+  
+  SS Versions   : 7.1, 7.0
+  
+  Copyright     : 2025 Thomas Creedon
+                  
+                  Tom's Web Consulting < http://www.tomsWeb.consulting/ >
+  
+  Changes
+  
+    * initial version
+    
+  */
