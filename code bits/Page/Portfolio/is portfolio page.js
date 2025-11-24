66db4c20c65f7de84b648836747db6b36d057503
Thomas Creedon <tom@tomsWeb.consulting>
2025-11-23 17:07:53 -0800

    initial version

---
+++ code bits/Page/Portfolio/is portfolio page.js
@@ -0,0 +1,36 @@
+// License < https://tinyurl.com/s872fb68 > | © 2025 Thomas Creedon | Tom's Web Consulting < http://www.tomsWeb.consulting/ >
+
+const isPortfolioPage = Static
+
+  .SQUARESPACE_CONTEXT
+  
+  .collection
+  
+  ?.type
+  
+  ===
+  
+  23;
+  
+/*
+
+  Example Usage :
+  
+    if ( ! isPortfolioPage ) return; // bail if not portfolio page
+    
+  
+  License       : < https://tinyurl.com/s872fb68 >
+  
+  Version       : 1.0.0
+  
+  SS Versions   : 7.1, 7.0
+  
+  Copyright     : 2025 Thomas Creedon
+                  
+                  Tom's Web Consulting < http://www.tomsWeb.consulting/ >
+  
+  Changes
+  
+    * initial version
+    
+  */
