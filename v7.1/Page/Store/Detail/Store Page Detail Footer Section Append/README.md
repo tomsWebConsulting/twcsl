6ff053f882171d2bb25eb8edab738e22c2726dea
Thomas Creedon <tom@tomsWeb.consulting>
2025-12-09 18:57:08 -0800

    updated to use elements manipulate
    bumped version to 0.2.0

---
+++ v7.1/Page/Store/Detail/Store Page Detail Footer Section Append/README.md
@@ -0,0 +1,101 @@
+# Store Page Detail Footer Section Append
+
+### [License][1]
+
+### Synopsis
+
+Append footer boilerplate sections after product detail section.
+
+### Version
+
+  * 0.2.0
+
+#### SS Version
+
+  * 7.1
+
+#### Products V2 Compatible
+
+  * Yes
+  
+#### Fluid Engine Compatible
+
+  * Yes
+
+#### Dependencies
+
+  * [Squarespace plan][2] that supports [JavaScript][3].
+  
+  * [Elements Manipulate][4]
+
+---
+
+## Install
+
+* Install **[Elements Manipulate][5]**. After you install the code be sure to
+  continue on with the rest of the steps.
+  
+* Options
+
+  * Page Specific
+  
+    * Use this option if you want to have this effect on only one Store page.
+      
+    * Add code from file **[store page detail footer section append.html][6]**
+      to Store Page Settings > Advanced > Page Header Code Injection for the
+      page. Refer to [per-page code injection][7] for details.
+      
+    * Add code from file **[store page detail footer section append.js][8]** to
+      the **elements manipulate options** code.
+      
+  * Site-wide
+  
+    * Use this option if you want to have this effect on all Store pages.
+      
+    * Add code from file **[store page detail footer section append.less][9]**
+      to Website > Pages > Custom Code > Custom CSS. Refer to [Using the CSS
+      Editor][10] for details.
+      
+    * Add code from file **[store page detail footer section append.js][8]** to
+      the **elements manipulate options** code.
+      
+* Add sections to the footer.
+
+  * Add a code block to each section and set the content to the following.
+    
+    ```html
+    <x-twc-spdfsa />
+    ```
+    
+    If the section is a Classic Editor section put the code block as the first
+    block of the section. Below the code block build up your boilerplate content
+    you want to be added to each product.
+    
+  * Save the page.
+
+## Make a Donation
+
+Please consider [making a donation][11].
+
+## Changes
+
+* **2025-12-09**
+
+  * updated to use elements manipulate
+  * bumped version to 0.2.0
+  
+* **2023-12-31**
+
+  * initial version
+
+[1]: https://github.com/tomsWebConsulting/twcsl/blob/main/LICENSE.txt#L1
+[2]: https://www.squarespace.com/pricing
+[3]: https://en.wikipedia.org/wiki/JavaScript
+[4]: https://github.com/tomsWebConsulting/twcsl/tree/main/Element/Elements%20Manipulate
+[5]: https://github.com/tomsWebConsulting/twcsl/tree/main/Element/Elements%20Manipulate#elements-manipulate
+[6]: store%20page%20detail%20footer%20section%20append.html#L1
+[7]: https://support.squarespace.com/hc/en-us/articles/205815908-Using-code-injection#h_01JGPDM34K9B0J2SNGJE936M7K
+[8]: store%20page%20detail%20footer%20section%20append.js#L1
+[9]: store%20page%20detail%20footer%20section%20append.less#L1
+[10]: https://support.squarespace.com/hc/en-us/articles/206545567-Using-the-CSS-Editor
+[11]: https://github.com/tomsWebConsulting/twcsl#make-a-donation
---
+++ v7.1/Page/Store/Detail/Store Page Detail Footer Section Append/store page detail footer section append.js
@@ -0,0 +1,72 @@
+        /*
+        
+          store page detail footer section append
+          
+          License     : < https://tinyurl.com/s872fb68 >
+          
+          Version     : 0.2.0
+          
+          SS Version  : 7.1
+          
+          Copyright   : 2023-2025 Thomas Creedon
+                        
+                        Tom's Web Consulting < http://www.tomsWeb.consulting/ >
+          
+          */
+          
+        'body.collection-type-products.view-item #footer-sections section[ data-test="page-section" ]:has( x-twc-spdfsa )' : {
+        
+          // optional - sourceAncestorSelector, use when you want to first find
+          // a source element but then manipulate an ancestor element
+          
+          sourceAncestorSelector : '[ optional - enter source ancestor selector here between single quotes replacing square brackets ]',
+          
+          // sourceCopy when true will make a copy of the source element and use
+          // it instead of the actual sourceSelector. value is false or true
+          
+          sourceCopy : true,
+          
+          // destinationSelector, use the selector x-twc-em-source to use the
+          // sourceSelector as the destination
+          
+          destinationSelector : '#page-regions',
+          
+          // optional - destinationAncestorSelector, use when you want to first
+          // find a destination element but then manipulate an ancestor element
+          
+          destinationAncestorSelector : '[ enter optional destination ancestor selector here between single quotes replacing square brackets ]',
+          
+          // optional - rootSelector, use when you want the source and
+          // destination elements to have a common ancestor element
+          
+          rootSelector : '[ optional - enter root selector here between single quotes replacing square brackets ]',
+          
+          // action value is after, append, before, prepend or replace
+          
+          action : 'append',
+          
+          // repeat is a number. the number of times to repeat this specific
+          // element manipulation
+          
+          repeat : 1,
+          
+          // onEditModeRemove value is true or false. when true the source
+          // element will be removed
+          
+          onEditModeRemove : true,
+          
+          // callbacks is optional, use when you want to manipulate the
+          // structure of the source element. the value is an array. you can add
+          // one or multiple callback names. when adding multiple callback names
+          // separate the names with commas. some examples...
+          //
+          // [ 'callback' ]
+          //
+          // [ 'callback1', 'callback2', 'callback3' ]
+          //
+          // your callbacks must accept and return a node
+          
+          callbacks : [ ]
+          
+          },
+          
