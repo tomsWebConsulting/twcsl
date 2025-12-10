152e63025ee7d549aa91684adb0f501cd87b2490
Thomas Creedon <tom@tomsWeb.consulting>
2025-12-10 10:56:00 -0800

    initial version

---
+++ v7.1/Bookmarklet/Pages Panel Expand All/README.md
@@ -0,0 +1,52 @@
+# Pages Panel Expand All
+
+### [License][1]
+
+### Synopsis
+
+Expand all pages in the pages panel.
+
+### Version
+
+  * 0.1.0
+
+#### SS Version
+
+  * 7.1
+
+---
+
+## Quick Install
+
+* Add code from file **[pages panel expand all.bookmarklet][2]** as the URL
+  for a bookmark in your browser named **TWC Pages Panel Expand All**.
+
+## How to Use
+
+* Navigate to the Pages panel of your site.
+
+* Use the bookmarklet.
+
+## What is a bookmarklet?
+
+Wikipedia has a nice entry on [bookmarklet][3].
+
+## Make a Donation
+
+Please consider [making a donation][4].
+
+## Changes
+
+<!-- * **2025-05-17**
+
+  * added randomize products button
+  * bumped version to 0.2.0
+  -->
+* **2025-12-10**
+
+  * initial version
+
+[1]: https://github.com/tomsWebConsulting/twcsl/blob/main/LICENSE.txt#L1
+[2]: pages%20panel%20expand%20all.bookmarklet#L1
+[3]: https://en.wikipedia.org/wiki/Bookmarklet
+[4]: https://github.com/tomsWebConsulting/twcsl#make-a-donation
---
+++ v7.1/Bookmarklet/Pages Panel Expand All/pages panel expand all.js
@@ -0,0 +1,81 @@
+( ( ) => {
+
+  /*
+  
+    Pages Panel Expand All
+    
+    License       : < https://tinyurl.com/s872fb68 >
+    
+    Version       : 0.1.0
+    
+    SS Version    : 7.1
+    
+    Copyright     : 2025 Thomas Creedon
+                    
+                    Tom's Web Consulting < http://www.tomsWeb.consulting/ >
+    
+    no user serviceable parts below
+    
+    */
+    
+  const
+  
+    version = '0.1.0',
+    
+    s = `
+    
+      Pages Panel Expand All v${ version }
+      
+      License < https://tinyurl.com/s872fb68 >
+      
+      © 2025 Thomas Creedon
+      
+      Tom's Web Consulting < http://www.tomsWeb.consulting >
+      
+      `
+      
+      .replace ( /^\s+/gm, '' );
+      
+  console.log ( s );
+  
+  const
+  
+    selectorPrefix = 'div[ data-test="pages-panel" ] button[ data-test="',
+    
+    selectorSuffix = 'collapse-arrow" ]:has( path[ '
+    
+      +
+      
+      'd="M6 7.5V10l5 5 5-5V7.5l-5 5-5-5z" ] )',
+      
+    selector
+    
+      =
+      
+      `${ selectorPrefix }${
+      
+        [
+        
+          'group-',
+          
+          ''
+          
+          ]
+          
+          .join ( `${ selectorSuffix }, ${ selectorPrefix }` )
+          
+        }${ selectorSuffix }`;
+        
+  window
+  
+    .top
+    
+    .document
+    
+    .body
+    
+    .querySelectorAll ( selector )
+    
+    .forEach ( e => e.click ( ) );
+    
+  } ) ( );
