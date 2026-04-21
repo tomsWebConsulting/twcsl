b25b9060bd7b2384d3286526f5798eb50fcd1386
Thomas Creedon <tom@tomsWeb.consulting>
2026-04-20 20:14:00 -0700

    initial version

---
+++ code bits/Page/is cart page.js
@@ -0,0 +1,30 @@
+// License < https://tinyurl.com/s872fb68 > | © 2026 Thomas Creedon | Tom's Web Consulting < http://www.tomsWeb.consulting/ >
+
+const isCartPage =
+
+  location.pathname === '/cart';
+
+/*
+
+  Example Usage :
+  
+    // bail if not cart page
+    
+    if ( ! isCartPage ) return;
+    
+  
+  License       : < https://tinyurl.com/s872fb68 >
+  
+  Version       : 1.0.0
+  
+  SS Versions   : 7.1, 7.0
+  
+  Copyright     : 2025 Thomas Creedon
+                  
+                  Tom's Web Consulting < http://www.tomsWeb.consulting/ >
+  
+  Changes
+  
+    * initial version
+    
+  */
