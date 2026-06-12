0f57c476d455a9913602660e881cb6bc8a894030
Thomas Creedon <tom@tomsWeb.consulting>
2026-06-12 14:38:34 -0700

    initial version

---
+++ Element/Link/Link Force No Open in New Window Tab/link force no open in new window tab.js
@@ -0,0 +1,79 @@
+( ( ) => {
+
+  // debugger;
+  
+  /*!
+  
+    link force no open in new window tab
+    
+    License           : < https://tinyurl.com/s872fb68 >
+    
+    Version           : 0.1.0
+    
+    SS Versions       : 7.1, 7.0
+    
+    Copyright         : 2026 Thomas Creedon
+                        
+                        Tom's Web Consulting
+                        
+                        < http://www.tomsWeb.consulting/ >
+    
+    no user serviceable parts below
+    
+    */
+    
+  const
+  
+    title = 'Link Force No Open in New Window Tab',
+    
+    version = '0.1.0',
+    
+    s = `${ title } v${ version }
+    
+      License < https://tinyurl.com/s872fb68 >
+      
+      © 2026 Thomas Creedon
+      
+      Tom's Web Consulting < http://www.tomsWeb.consulting >`
+      
+      .replace ( /^\s+/gm, '' );
+      
+  console.log ( s );
+  
+  const domContentLoadedCallback =
+  
+    ( ) => {
+    
+      document
+      
+        .body
+        
+        .querySelectorAll ( 'a' )
+        
+        .forEach (
+        
+          e => e.removeAttribute (
+          
+            'target'
+            
+            )
+            
+          );
+          
+      };
+      
+  // domContentLoadedCallback ( );
+  
+  //
+  
+  document.addEventListener (
+  
+    'DOMContentLoaded',
+    
+    domContentLoadedCallback
+    
+    );
+    
+  //
+  
+  } ) ( );
